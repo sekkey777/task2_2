2a6c233f1d3dd6c526b95096fa4174a2dcd7e7cc
sekkey777 <seki7united@gmail.com>
2022-09-08 06:40:38 +0900

    0押下時の挙動について修正
    ・00.1などを入力できないよう修正
    ・/記号の後は0を入力できないように修正

--- main.js
+++ main.js
@@ -3,13 +3,17 @@
 let result = "";
 const multi = document.querySelector('.multi');
 const division = document.querySelector('.div');
-const dobule_zero = document.querySelector('.zero');
+const number = document.querySelectorAll('.number');
+const zero = document.querySelector('.zero');
+const dobule_zero = document.querySelector('.double_zero');
 const dot = document.querySelector('.dot');
 
 /* 数値が押された時 */
 function num_button(btn_num) {
     multi.disabled = false;
     division.disabled = false;
+    number.forEach( number => number.disabled =false);
+    zero.disabled = false;
     dobule_zero.disabled = false;
     dot.disabled = false;
     if(result === "") {
@@ -22,10 +26,35 @@ function num_button(btn_num) {
     }
 }
 
+/* 0が押された時 */
+function zero_button(zero_num) {
+    multi.disabled = false;
+    division.disabled = false;
+    dot.disabled = false;
+    if(result === "") {
+        result = zero_num;
+        console.log(number)
+        number.forEach( number => number.disabled =true);
+        zero.disabled = true;
+        dobule_zero.disabled = true;
+        console.log(1)
+        document.getElementById('display').textContent = result;
+    }
+    else if(result.slice(-1) === "/") {
+        document.getElementById('display').textContent = result;
+    }
+    else if(result !== "") {
+        result += zero_num;
+        document.getElementById('display').textContent = result;
+    }
+}
+
 /* 演算子（＋ー＊／）が押された時 */
 function operator(btn_sign) {
     result = document.getElementById('display').textContent;
     x = result.slice(-1);
+    zero.disabled = false;
+    number.forEach( number => number.disabled =false);
     if(x!=="+" && x!=="-" && x!=="*" && x!=="/" && x!==".") {
         console.log(1);
         result += btn_sign;
@@ -43,6 +72,8 @@ function reset() {
     result = "";
     multi.disabled = true;
     division.disabled = true;
+    number.forEach( number => number.disabled =false);
+    zero.disabled = false;
     dobule_zero.disabled = true;
     dot.disabled = true;
     document.getElementById('display').textContent = result;
